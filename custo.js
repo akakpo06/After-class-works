2d155fa696778535ff7080280d2c64cd35a23110
akakpo06 <godwinakakpo023@gmail.com>
2022-06-16 12:22:25 +0000

    fonctionnalité de scroll sur tablette et mobile

---
+++ custo.js
@@ -0,0 +1,18 @@
+let board = [
+    `/Assets/Images/Presentation-plat1.jpg`,
+    `/Assets/Images/Presentation-plat-2.jpg`,
+    `/Assets/Images/Presentation-plat-3.jpg`
+]
+let position = 0
+function moveTo(n) {
+    position += n
+    if (position === -1) {
+        position = 2
+    }
+    else if (position === 3) {
+        position = 0
+    }
+
+    document.getElementById("image-cont").innerHTML = `<img src="${board[position]}" alt="">`
+    
+}
